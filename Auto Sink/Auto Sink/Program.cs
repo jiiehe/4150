af1fa95d7420c36f4f3933248bdc14715efbd13e
jinh <jinh@eng.utah.edu>
2017-09-29 14:29:59 -0600

    need to finish DFS and finish assignment

--- Auto Sink/Auto Sink/Program.cs
+++ Auto Sink/Auto Sink/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,38 @@ using System.Threading.Tasks;
 
 namespace Auto_Sink
 {
+    class City : IComparable
+    {
+        private String name;
+        private int charge;
+        private HashSet<String> highways;
+        public City(String name, int charge)
+        {
+            this.name = name;
+            this.charge = charge;
+            this.highways = new HashSet<string>();
+        }
+        public void addNewWay(String name)
+        {
+            highways.Add(name);
+        }
+
+        public int CompareTo(object obj)
+        {
+            City other = (City)obj;
+            if (this.charge > other.charge)
+            {
+                return 1;
+            }else
+            {
+                return 0;
+            }
+        }
+    }
+
+
+
+
     class Program
     {
         static void Main(string[] args)
